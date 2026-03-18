942852ab8b19f3ba6bc1b952a139a0ae301d5e36
Osvaldo Acosta <osvaldo.arco0794@gmail.com>
2026-03-18 12:59:27 -0600

    Carga de informacion por ID

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,16 +1,23 @@
-interface PokemonPageProps {
-    params: Promise<{
-        id: string;
-    }>;
+import { Pokemon } from "../../pokemons";
+interface Props {
+    params: Promise<{id: string}>
 }
-
-export default async function PokemonPage({ params }: PokemonPageProps) {
-    // Extract the id from the params object
+const getPokemon = async (id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        cache: 'force-cache'
+    }).then(res => res.json())
+    console.log('Se cargo: ' + pokemon.name)
+    return pokemon
+}
+export default async function PokemonPage({ params }: Props) {
     const { id } = await params;
-
+    const pokemon = getPokemon(id)
     return (
         <div>
-            <h1>Pokemon: {id}</h1>
+            <h1>Pokemon: {(await pokemon).id}</h1>
+            <div>
+                { JSON.stringify((await pokemon))}
+            </div>
         </div>
     );
 }
--- app/dashboard/pokemons/index.ts
+++ app/dashboard/pokemons/index.ts
@@ -1,4 +1,6 @@
 export type { PokemonsResponse } from './interfaces/pokemons-response'
 export type { SimplePokemons } from './interfaces/simple-pokemons'
+export type { Pokemon } from './interfaces/pokemon'
+
 export { PokemonGrid } from './components/PokemonGrid'
 export { PokemonCard } from './components/PokemonCard'
